3168480a9204261380f5f351d2c56fd257958561
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-08-19 16:02:46 +0700

    Feat : first migrate

---
+++ prisma/migrations/20250819090210_add_user/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
